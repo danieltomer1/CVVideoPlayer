ab1f7cf51082efd092a7c0ff4ba932c7966ccc74
Achiya Zigler <achiya@sightx.ai>
2024-04-14 19:46:39 +0300

    Extract InputManager to a separate class

--- cvvideoplayer/__init__.py
+++ cvvideoplayer/__init__.py
@@ -3,3 +3,4 @@ from .recorder import Recorder
 from .video_player import VideoPlayer
 from .utils.bbox_utils import Bbox
 from .utils.video_player_utils import KeyFunction
+from .input_manager import InputManager
