ead9e0fc0a6e1fe9a680d21726f954c9286e2c44
Daniel <danieltomer1@gmail.com>
2024-11-02 20:39:44 +0200

    add demos folder

--- cvvideoplayer/frame_editors/__init__.py
+++ cvvideoplayer/frame_editors/__init__.py
@@ -4,3 +4,4 @@ from .basic_frame_editors.frame_normlizer import FrameNormalizer
 from .basic_frame_editors.frame_info_overlay import FrameInfoOverlay
 from .basic_frame_editors.histogram_equalizer import HistogramEqualizer
 from .basic_frame_editors.key_map_overlay import KeyMapOverlay
+from .optical_flow_plotter import OpticalFlowPlotter
---
+++ demos/optical_flow.py
@@ -0,0 +1,25 @@
+from demo_utils import change_cwd_to_demos_dir, add_project_root_to_path
+change_cwd_to_demos_dir()
+add_project_root_to_path()
+
+from cvvideoplayer import VideoPlayer
+from cvvideoplayer.frame_editors import FrameInfoOverlay, KeyMapOverlay, OpticalFlowPlotter
+
+
+def run_player():
+    video_player = VideoPlayer(
+        video_source="../assets/example_video.mp4",
+        record=True,
+        frame_edit_callbacks=[
+            FrameInfoOverlay(),
+            KeyMapOverlay(),
+            OpticalFlowPlotter(enable_by_default=True),
+        ]
+    )
+
+    with video_player:
+        video_player.run()
+
+
+if __name__ == "__main__":
+    run_player()
